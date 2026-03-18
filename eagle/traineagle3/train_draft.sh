f0a4e73dc68efdb7a883351fe231ce7b086ab398
Y M Kim <ymkim@mock.com>
2026-03-18 14:52:21 +0900

    Set HF cache paths to persistent EAGLE folder to avoid losing cache on restart

--- eagle/traineagle3/train_draft.sh
+++ eagle/traineagle3/train_draft.sh
@@ -4,6 +4,10 @@
 
 cd "$(dirname "$0")"
 
+# Set HuggingFace cache directories to the persistent project folder
+export HF_DATASETS_CACHE="/group-volume/ym1012.kim/homepc/EAGLE/.cache/huggingface/datasets"
+export HUGGINGFACE_HUB_CACHE="/group-volume/ym1012.kim/homepc/EAGLE/.cache/huggingface/hub"
+
 python -m deepspeed.launcher.runner main.py \
     --deepspeed_config ds_config.json \
     --basepath ../../Llama-3.1-8B-Instruct \
